d05cf880e071f131bb4edeca9cae475304323da8
CodingWorker <dy10010@sina.cn>
2017-09-25 21:50:21 +0800

    add

---
+++ codes/src/main/java/com/test/elasticsearch/Main.java
@@ -0,0 +1,53 @@
+package com.test.elasticsearch;
+
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
+/**
+ * Created by IntelliJ IDEA
+ * User:    DaiYan
+ * Date:    2017/9/25
+ * Project: elasticsearchdemo
+ */
+public class Main {
+    private static Client client;
+    public static void main(String[] args) throws Exception{
+        connect();
+        index();
+    }
+
+    private static void connect()throws Exception{
+        Settings settings = Settings.builder().put("cluster.name", "elasticsearch").build();
+        try {
+            //创建client
+             client = new PreBuiltTransportClient(settings)
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("172.17.50.239"), 9300));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void close(){
+        //关闭client
+        client.close();
+    }
+
+    private static void index()throws Exception{
+//        GetResponse response = client.prepareGet("blog", "article", "1").execute().actionGet();
+//        System.out.println(response.getSourceAsString());
+        GetRequest getRequest=new GetRequest("test");
+        IndexResponse response= client.prepareIndex("test","type-test",null)
+                .setSource("{\"content\":\"hello\"}")
+                .get();
+        System.out.println(response.status());
+    }
+}
